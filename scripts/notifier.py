3ebba82abb5c78f61ac801e393fc7c89465d7d59
SmileMaskL <143575228+SmileMaskL@users.noreply.github.com>
2025-05-15 18:51:54 +0900

    Update notifier.py

--- scripts/notifier.py
+++ scripts/notifier.py
@@ -7,10 +7,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def send_notification(message: str):
-    """
-    Slack 알림 전송 함수
-    환경변수 SLACK_API_TOKEN, SLACK_CHANNEL에서 자동 토큰과 채널을 가져옴
-    """
     slack_token = os.getenv("SLACK_API_TOKEN")
     slack_channel = os.getenv("SLACK_CHANNEL")
 
