62d585ccdb2bd3502352bd163ba8cc2ed1da7385
SmileMaskL <143575228+SmileMaskL@users.noreply.github.com>
2025-05-16 15:13:08 +0900

    Create maintenance.py

---
+++ scripts/maintenance.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+def cleanup_old_files(directory=".", days=7):
+    now = time.time()
+    cutoff = now - (days * 86400)
+
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            if os.path.getmtime(filepath) < cutoff:
+                os.remove(filepath)
